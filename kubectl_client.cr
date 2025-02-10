008d6cbb6ed88617189459f06579db3eda614c49
Rafal Lal <rafal.lal@tietoevry.com>
2025-02-10 16:24:29 +0000

    Refactor KubectlClient module without ::Get
    Signed-off-by: Rafal Lal <rafal.lal@tietoevry.com>

--- kubectl_client.cr
+++ kubectl_client.cr
@@ -1,14 +1,17 @@
 require "totem"
 require "colorize"
 require "docker_client"
-require "./src/modules/get.cr"
-require "./src/modules/modules.cr"
+require "./src/modules/*"
 require "./src/utils/utils.cr"
 require "./src/utils/system_information.cr"
 
 module KubectlClient
+  Log = ::Log.for("k8s-client")
+
   alias K8sManifest = JSON::Any
   alias K8sManifestList = Array(JSON::Any)
+  alias CMDResult = NamedTuple(status: Process::Status, stdout: String, stderr: String)
+  alias BackgroundCMDResult = NamedTuple(process: Process, stdout: String, stderr: String)
 
   WORKLOAD_RESOURCES = {deployment:      "Deployment",
                         service:         "Service",
@@ -18,9 +21,11 @@ module KubectlClient
                         daemonset:       "DaemonSet",
                         service_account: "ServiceAccount"}
 
-  module ShellCmd
-    def self.run(cmd, log_prefix, force_output = false)
-      Log.info { "#{log_prefix} command: #{cmd}" }
+  module ShellCMD
+    # logger should have method name (any other scopes, if necessary) that is calling attached using .for() method.
+    def self.run(cmd, logger : ::Log = Log, force_output = false) : CMDResult
+      logger = logger.for("cmd")
+      logger.debug { "command: #{cmd}" }
       status = Process.run(
         cmd,
         shell: true,
@@ -28,20 +33,22 @@ module KubectlClient
         error: stderr = IO::Memory.new
       )
       if force_output == false
-        Log.debug { "#{log_prefix} output: #{output.to_s}" }
+        logger.debug { "output: #{output.to_s}" }
       else
-        Log.info { "#{log_prefix} output: #{output.to_s}" }
+        logger.info { "output: #{output.to_s}" }
       end
 
       # Don't have to output log line if stderr is empty
       if stderr.to_s.size > 1
-        Log.info { "#{log_prefix} stderr: #{stderr.to_s}" }
+        logger.warn { "stderr: #{stderr.to_s}" }
       end
-      {status: status, output: output.to_s, error: stderr.to_s}
+
+      CMDResult.new(status: status, output: output.to_s, error: stderr.to_s)
     end
 
-    def self.new(cmd, log_prefix, force_output = false)
-      Log.info { "#{log_prefix} command: #{cmd}" }
+    def self.new(cmd, logger : ::Log = Log, force_output = false) : CMDResult
+      logger = logger.for("cmd-background")
+      logger.debug { "command: #{cmd}" }
       process = Process.new(
         cmd,
         shell: true,
@@ -49,29 +56,32 @@ module KubectlClient
         error: stderr = IO::Memory.new
       )
       if force_output == false
-        Log.debug { "#{log_prefix} output: #{output.to_s}" }
+        logger.debug { "output: #{output.to_s}" }
       else
-        Log.info { "#{log_prefix} output: #{output.to_s}" }
+        logger.info { "output: #{output.to_s}" }
       end
 
       # Don't have to output log line if stderr is empty
       if stderr.to_s.size > 1
-        Log.info { "#{log_prefix} stderr: #{stderr.to_s}" }
+        logger.warn { "stderr: #{stderr.to_s}" }
       end
-      {process: process, output: output.to_s, error: stderr.to_s}
+
+      BackgroundCMDResult.new(process: process, output: output.to_s, error: stderr.to_s)
     end
   end
 
-  def self.installation_found?(verbose = false, offline_mode = false)
+  def self.installation_found?(verbose = false, offline_mode = false) : Bool
     kubectl_installation(verbose = false, offline_mode = false).includes?("kubectl found")
   end
 
-  def self.server_version
-    Log.debug { "KubectlClient.server_version" }
-    result = ShellCmd.run("kubectl version --output json", "KubectlClient.server_version", true)
+  def self.server_version : String
+    logger = Log.for("server_version")
+
+    result = ShellCMD.run("kubectl version --output json", logger)
     version = JSON.parse(result[:output])["serverVersion"]["gitVersion"].as_s
     version = version.gsub("v", "")
-    Log.info { "KubectlClient.server_version: #{version}" }
+
+    logger.info { "K8s server version is: #{version}" }
     version
   end
 end
--- src/utils/constants.cr
+++ src/utils/constants.cr
@@ -1,6 +1,7 @@
 module KubectlClient
-  DEFAULT_LOCAL_BINARY_PATH = "tools/git/linux-amd64/docker"
-  BASE_CONFIG               = "./config.yml"
+  DEFAULT_LOCAL_BINARY_PATH  = "tools/git/linux-amd64/docker"
+  BASE_CONFIG                = "./config.yml"
+  RESOURCE_WAIT_LOG_INTERVAL = 10
   # https://www.capitalone.com/tech/cloud/container-runtime/
   OCI_RUNTIME_REGEX = /containerd|docker|podman|runc|railcar|crun|rkt|gviso|nabla|runv|clearcontainers|kata|cri-o/i
 end
